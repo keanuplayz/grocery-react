ee264ce7f6d6c85fe8872d34c1d250664973cff1
Keanu <keanutimmermans8@gmail.com>
2020-06-14 11:49:04 +0200

    Added basic files.

---
+++ src/ShoppingList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class ShoppingList extends Component {
+    render() {
+        return (
+            <div className="ShoppingListMain">
+                <div className="header">
+                    <form>
+                        <input>
+                        </input>
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ShoppingList;
